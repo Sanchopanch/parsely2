d36faa76e4deb504969da6968bdae9b1949f0dc4
kochetkov <a.kochetkov@robo.cash>
2023-06-12 15:05:45 +0500

    ok

--- graph.py
+++ graph.py
@@ -7,43 +7,38 @@ import random
 from matplotlib.ticker import MultipleLocator
 
 def diagr(word):
-    number_of_colors = 200
-    color = ["#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
-                 for i in range(number_of_colors)]
 
-
-    # df = pd.read_csv('queueTable.csv')
     sqlite_connection = sqlite3.connect('baseSerp.db', timeout=10)
     df = pd.read_sql_query(f'SELECT se.date,se.num, se.site FROM serp as se, querries as q\
-                           where q.id= se.querry and q.querry="{word}"', sqlite_connection)
+                           where q.id= se.querry and q.querry="{word}" \
+                           --and se.date>"2023-06-11"', sqlite_connection)
     sqlite_connection.close()
 
 
-
-
-    df = df.assign(F = 3.0)
-    df.loc[df['site']=='kochetkov.spb.ru', 'F'] = 5.0
-    # df.loc[df['site']=='kochetkob.spb.ru', 'site'] = 16599713015387340
-
     sns.set_theme(style="ticks")
-    # sns.set_style("darkgrid")
     f, ax = plt.subplots()
-    plt.xticks(rotation = 'vertical')
+    plt.xticks(rotation='vertical')
     plt.rcParams['legend.fontsize'] = 6
 
     unique = df["site"].unique()
-    palette = dict(zip(unique, sns.color_palette(n_colors=len(unique))))
-    # palette.update({"Total": "k"})
-    palette['kochetkov.spb.ru'] = (0., 0., 0.)
+    hue_order = unique.tolist()
+    if 'kochetkov.spb.ru' in hue_order:
+        hue_order.remove('kochetkov.spb.ru')
+    hue_order.insert(0,'kochetkov.spb.ru')
+    palette = dict()
+    for key in unique:
+        palette[key] = (0., random.random(), random.random())
+    palette['kochetkov.spb.ru'] = (.9, 0., 0.)
+
 
     ax.tick_params(axis='x', rotation=90)
     g = sns.relplot(
         data=df,
-        x="date", y="num", sizes="F",
+        x="date", y="num", #size="F",
         hue="site",# col="F",
         kind="line", palette=palette,
-        height=10, aspect=0.7, facet_kws=dict(sharex=False), linewidth=3.5, legend='full'
-
+        height=10, aspect=0.7, facet_kws=dict(sharex=False), linewidth=3.5, legend='brief'
+        ,hue_order=hue_order
     )
 
     g.axes[0][0].invert_yaxis()
@@ -52,7 +47,7 @@ def diagr(word):
 
     for i, ax in enumerate(g.fig.axes):   ## getting all axes of the fig object
          ax.set_xticklabels(ax.get_xticklabels(), rotation=90)
-    # plt.gridd()
+
     ax.yaxis.set_major_locator(MultipleLocator(1))
     plt.show()
     g.fig.show()
---
+++ plshow.py
@@ -0,0 +1,20 @@
+import plotly.express as px
+import pandas as pd
+import sqlite3
+
+word = 'kochetkov spb'
+
+df = px.data.gapminder().query("country in ['Canada', 'Botswana']")
+sqlite_connection = sqlite3.connect('baseSerp.db', timeout=10)
+df = pd.read_sql_query(f'SELECT se.date,se.num, se.site FROM serp as se, querries as q\
+                       where q.id= se.querry and q.querry="{word}" \
+                       --and se.date>"2023-06-11"', sqlite_connection)
+sqlite_connection.close()
+
+fig = px.line(df, x="date", y="num", color="site", markers=True)
+fig.update_traces(textposition="bottom right")
+fig.update_layout(yaxis=dict(autorange="reversed", title=word))
+fig.update_yaxes(dtick=1, showgrid=True)
+
+fig.update_traces(marker_line_width=1, marker_size=15, line_width=10)
+fig.show()
