84cfdfb2ce2a2b61f0dc076ea2e38cbed9038316
kochetkov <a.kochetkov@robo.cash>
2023-06-17 10:26:59 +0500

    koi

--- plshow.py
+++ plshow.py
@@ -1,18 +1,36 @@
 import plotly.express as px
 import pandas as pd
 import sqlite3
+import random as rnd
 
-word = 'kochetkov'
+word = 'kochetkov spb'
+my_sites = ['kochetkov.spb.ru', 'теплорасчет.рф']
 
 df = px.data.gapminder().query("country in ['Canada', 'Botswana']")
 sqlite_connection = sqlite3.connect('baseSerp.db', timeout=10)
-df = pd.read_sql_query(f'SELECT se.date,se.num, se.site FROM serp as se, querries as q\
+df = pd.read_sql_query(f'SELECT se.date,se.num, se.site  \
+                       ,case when se.site in {str(my_sites).replace("[","(").replace("]",")")} then 1 else 0 end as ord\
+                       FROM serp as se, querries as q    \
                        where q.id= se.querry and q.querry="{word}" \
-                       --and se.date>"2023-06-11"', sqlite_connection)
+                       order by ord \
+                       --and se.date>"2023-06-11" \
+                       \
+                       ', sqlite_connection)
 sqlite_connection.close()
+unique = df["site"].unique()
+num_yours, num_others = 0, 0
+palette = []
+for s in unique:
+    num_yours =   num_yours + 1 if     s in my_sites else num_yours
+    num_others = num_others + 1 if not s in my_sites else num_others
+
+for _ in range(num_others):
+    palette.append(rnd.choice(px.colors.qualitative.Pastel2))
+for i,_ in enumerate(range(num_yours)):
+    palette.append(px.colors.qualitative.Plotly[i])
 
 fig = px.line(df, x="date", y="num", color="site", markers=True, title=word,
-              color_discrete_sequence=px.colors.qualitative.Pastel
+              color_discrete_sequence=palette
               )
 fig.update_traces(textposition="bottom right")
 fig.update_layout(yaxis=dict(autorange="reversed", title=word))
