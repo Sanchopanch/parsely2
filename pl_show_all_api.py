dd882495e91846088b78d449b2fc544e955d2386
kochetkov <a.kochetkov@robo.cash>
2023-06-23 10:41:23 +0500

    del

--- pl_show_all_api.py
+++ pl_show_all_api.py
@@ -11,8 +11,8 @@ def plot_word(fig, word, row, my_sites):
     start_date = '2023-06-01'
 
     ms_add = ' '.join([f'&my_sites={ms}' for ms in my_sites])
-    # response = requests.get(f"http://192.168.1.33:8880/?start_date={start_date}&word={word}{ms_add}")
-    response = requests.get(f"http://127.0.0.1:8880/?start_date={start_date}&word={word}{ms_add}")
+    response = requests.get(f"http://192.168.1.33:8880/?start_date={start_date}&word={word}{ms_add}")
+    # response = requests.get(f"http://127.0.0.1:8880/?start_date={start_date}&word={word}{ms_add}")
     if response.ok:
         data = response.json()['data']
         df = pd.DataFrame(data)
